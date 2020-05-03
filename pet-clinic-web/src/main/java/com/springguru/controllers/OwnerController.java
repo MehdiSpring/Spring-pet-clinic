e6893fd65dcc8b72688b7263564bd36d637ba2ec
MehdiSpring <mehdi.boumzi@gmail.com>
2020-05-04 01:11:33 +0200

    Owner Index Page and Controller are created. closes #13

---
+++ pet-clinic-web/src/main/java/com/springguru/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package com.springguru.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+	@RequestMapping({"","/","/index","/index.html"})
+	public String indexOwner()
+	{
+		return "owners/index";
+	}
+}
