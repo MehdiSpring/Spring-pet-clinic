6fb8b72ba1a7d5fe1b4b32552e0d3f89dba5e485
MehdiSpring <mehdi.boumzi@gmail.com>
2020-05-07 20:52:07 +0200

    Vets listed on the Vet index page. closes #15.

--- pet-clinic-web/src/main/java/com/springguru/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/springguru/controllers/VetController.java
@@ -1,15 +1,25 @@
 package com.springguru.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springguru.service.VetService;
 
 @RequestMapping("/vet")
 @Controller
 public class VetController {
+	
+	private final VetService vetService;
+	
+	public VetController(VetService vetService) {
+		this.vetService = vetService;
+	}
 
 	@RequestMapping({"","/","/index","/index.html"})
-	public String vetIndex()
+	public String vetIndex(Model model)
 	{
+		model.addAttribute("vets", this.vetService.findAll());
 		return "vet/index";
 	}
 }
