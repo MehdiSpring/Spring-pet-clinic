eb1d59eeef9748087f4ff392b7868382b12aafe2
MehdiSpring <mehdi.boumzi@gmail.com>
2020-05-07 20:41:11 +0200

    Owners listed on the Owner Index Page. closes #16.

--- pet-clinic-web/src/main/java/com/springguru/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/springguru/controllers/OwnerController.java
@@ -1,15 +1,27 @@
 package com.springguru.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springguru.service.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+	private final OwnerService ownerService;
+	
+	
+	public OwnerController(OwnerService ownerService) {
+		this.ownerService = ownerService;
+	}
+
+
 	@RequestMapping({"","/","/index","/index.html"})
-	public String indexOwner()
+	public String listAllOwners(Model model)
 	{
+		model.addAttribute("owners", this.ownerService.findAll());
 		return "owners/index";
 	}
 }
