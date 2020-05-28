1fd5e059379fd071024cc4cf7be3af7792dd53af
MehdiSpring <mehdi.boumzi@gmail.com>
2020-05-28 12:25:09 +0200

    Base Entity has been implemented. closes #35.

--- pet-clinic-data/src/main/java/com/springguru/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/springguru/model/BaseEntity.java
@@ -2,8 +2,20 @@ package com.springguru.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/*
+ * this annotation tells JPA to not create a table for this class, and all its
+ * informations and its mappings will be inherited by the subclasses
+ */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
