049936f6bc27dbd42cdc89318e378c49e239b4f8
MehdiSpring <mehdi.boumzi@gmail.com>
2020-05-28 15:33:00 +0200

    The JPA Service for owners has been created. closes #39.

--- pet-clinic-data/src/main/java/com/springguru/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/springguru/repositories/OwnerRepository.java
@@ -1,9 +1,12 @@
 package com.springguru.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.springguru.model.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long>{
 
+	Optional<Owner> findByLastName(String lastName);
 }
---
+++ pet-clinic-data/src/main/java/com/springguru/service/jpa/OwnerServiceJPAImpl.java
@@ -0,0 +1,89 @@
+package com.springguru.service.jpa;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.springguru.model.Owner;
+import com.springguru.repositories.OwnerRepository;
+import com.springguru.service.OwnerService;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerServiceJPAImpl implements OwnerService {
+
+	private final OwnerRepository ownerRepository;
+	
+	public OwnerServiceJPAImpl(OwnerRepository ownerRepository) {
+		this.ownerRepository = ownerRepository;
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		
+		//SInce Spring 5, this is returning an Optional type
+		Optional<Owner> owner = this.ownerRepository.findById(id);
+		
+		if(owner.isPresent())
+			return owner.get();
+		else
+			return null;
+	}
+
+	@Override
+	public Owner save(Owner object) {
+		// TODO Auto-generated method stub
+		return this.ownerRepository.save(object);
+	}
+
+	@Override
+	public Set<Owner> findAll() {
+		// TODO Auto-generated method stub
+	    Set<Owner> setOwners = new HashSet<Owner>();
+	    
+	    /*Iterator<Owner> owners= this.ownerRepository.findAll().iterator();
+	    
+	    while(owners.hasNext())
+	    {
+	    	setOwners.add(owners.next());
+	    }
+	    
+	    return setOwners;*/
+	    
+	    //This is a new way to use the lampbda expression
+	    this.ownerRepository.findAll().forEach(setOwners::add);
+	    
+	    return setOwners;
+	}
+
+	@Override
+	public void delete(Owner object) {
+		// TODO Auto-generated method stub
+
+		this.ownerRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		this.ownerRepository.deleteById(id);
+
+	}
+
+	@Override
+	public Owner findByLastname(String name) {
+		// TODO Auto-generated method stub
+		Optional<Owner> owner = this.ownerRepository.findByLastName(name);
+		
+		if(owner.isPresent())
+			return owner.get();
+		else
+			return null;
+	}
+
+}
